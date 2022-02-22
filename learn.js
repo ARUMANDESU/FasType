b74b48e4a443f25ea06404f4a02575dd5e6d0eb1
Arumandesu <zh.arumandes@gmail.com>
2022-02-22 16:48:30 +0600

    nothing important

--- learn.js
+++ learn.js
@@ -1,6 +1,9 @@
-var $header = $("header");
-$header.hide();
+var header = document.getElementsByClassName('header')[0];
+header.hide();
+window.onload = function() {
+  setTimeout(function() {
+    header.slideDown(1000);           
+  }, 1000);  
+} 
 
-setTimeout(function() {
-  $header.slideDown(1000);           
-}, 1000);
+alert();
