1d178cbbb9ca6a0774407f007607e7e582a0b6d6
Arumandesu <zh.arumandes@gmail.com>
2022-03-07 10:53:39 +0600

    acoount comment section

---
+++ account.js
@@ -0,0 +1,8 @@
+document.getElementById("inputText").onclick= function (){addComment()};
+
+function addComment(){
+    document.getElementById("comment").innerHTML+= '<div class="comments">' +
+        '<div class="raw my-2 p-2 d-flex" style="background-color: #264653;">' +
+        '<div class="col-2">' + '<img src="image/ecce-homo.jpg">' + '</div>' +
+        ' <div class="col-10">' + '<p>'+document.getElementById("inputComment").value+'</p>' + '</div>' + '</div>' + '</div>';
+}
