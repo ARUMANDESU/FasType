6a46a432c4a832e518bdcf4198d9d3a72dc93bcd
Arumandesu <zh.arumandes@gmail.com>
2022-03-07 11:46:13 +0600

    lol

--- account.js
+++ account.js
@@ -5,4 +5,5 @@ function addComment(){
         '<div class="raw my-2 p-2 d-flex" style="background-color: #264653;">' +
         '<div class="col-2">' + '<img src="image/ecce-homo.jpg">' + '</div>' +
         ' <div class="col-10">' + '<p>'+document.getElementById("inputComment").value+'</p>' + '</div>' + '</div>' + '</div>';
+    document.getElementById("inputComment").value="";
 }
